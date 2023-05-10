507ccc16a531b84d20edb6adb0e17760b7cde75d
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 13:34:46 -0300

    add methods for load array values in model

--- public/index.php
+++ public/index.php
@@ -3,11 +3,6 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-// Teste metodo getResultFromQuery
-$sql = "SELECT * FROM users";
-$result = Database::getResultFromQuery($sql);
-
-while ($row = $result->fetch_assoc()) {
-  print_r($row);
-  echo '<br>';
-}
+$user = new User(['name' => 'Bruno', 'email' => 'bruno@super.com.br']);
+print_r($user);
+echo 'sucesso';
