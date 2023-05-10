2ae66c205d89c9bd73cb08c1f4929bcc39ba5cc6
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 13:38:53 -0300

    add magic methods(get,set) in model

--- public/index.php
+++ public/index.php
@@ -4,5 +4,6 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Bruno', 'email' => 'bruno@super.com.br']);
-print_r($user);
+print_r($user->email);
+// print_r($user->get('email'));
 echo 'sucesso';
