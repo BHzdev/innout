7a6b959246199cfdc12b3a36166efecb8e9c1c15
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 13:26:12 -0300

    add config.php and models, model and user, some configurations and definitions of constants

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 // Teste metodo getResultFromQuery
 $sql = "SELECT * FROM users";
