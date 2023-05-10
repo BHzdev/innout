14b7c10e52544dd5dbedee0ad499398adf3b7cb5
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 15:09:42 -0300

    add paste controllers, add login controller

--- public/index.php
+++ public/index.php
@@ -1,19 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-// require_once(VIEW_PATH . '/login.php');
-
-// Teste checkLogin
-require_once(MODEL_PATH . '/Login.php');
-
-$login = new Login([
-  'email' => 'admin@cod3r.com.br',
-  'password' => 'a'
-]);
-
-try {
-  $login->checkLogin();
-  echo "Usuario logado com sucesso :0";
-} catch (Exception $e) {
-  echo "Problema no login :P";
-};
+require_once(CONTROLLER_PATH . '/login.php');
