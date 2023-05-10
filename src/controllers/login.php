6b51a5864b9aaae126ce13fae5a0560fd9810302
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 15:26:49 -0300

    add loader.php, add to load model and view, login controller working

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "login controller...";
+loadModel('Login');
+if (count($_POST)) {
+  $login = new Login($_POST);
+  try {
+    $user = $login->checkLogin();
+    echo "Usuario {$user->name} logado";
+  } catch (Exception $e) {
+    echo "Falha no login";
+  }
+}
+loadView("login", $_POST);
