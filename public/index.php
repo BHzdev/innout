2f55add1788af1cb0596a23877fc2dfb7ef6324b
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-10 14:19:26 -0300

    add methods(getSelect, getFilters and getFormatedValue) on model to execute select queries

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Bruno', 'email' => 'bruno@super.com.br']);
-print_r($user->email);
-// print_r($user->get('email'));
-echo 'sucesso';
+// $user = new User(['name' => 'Bruno', 'email' => 'bruno@super.com.br']);
+// echo $user->getSelect();
+
+echo User::getSelect(['id' => 1], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@super.com.br']);
