b501d026ed3cdbaf047b383348074ccffaaa5f47
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-11 18:41:03 -0300

    adding login redirect

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -7,7 +7,7 @@ if (count($_POST)) {
   $login = new Login($_POST);
   try {
     $user = $login->checkLogin();
-    echo "Usuario {$user->name} logado";
+    header('Location: day_records.php');
   } catch (AppException $e) {
     $exception = $e;
   }
