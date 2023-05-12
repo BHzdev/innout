1f45218b70c74eb77594be36c36e6bbec5382607
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-11 22:34:08 -0300

    pulling user information via sessions and implementing in header

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -27,8 +27,9 @@
     <div class="spacer"></div>
     <div class="dropdown">
       <div class="dropdown-button">
+        <img class="avatar" src="<?= "http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="user">
         <span class="ml-3">
-          Usúario Mock
+          <?= $_SESSION['user']->name ?>
         </span>
         <i class="icofont-simple-down mx-2"></i>
       </div>
