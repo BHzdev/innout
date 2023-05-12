187a62ec51dcfeab93b840309f58187d5e2d049f
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-11 23:42:20 -0300

    feat: add data_generator, function returns an array depending on the probability passed by the parameters

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,10 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
-// Definição de constantes
+// Definição de constantes gerais
+define('DAILY_TIME', 60 * 60 * 8);
+
+// Definição de constantes das pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
