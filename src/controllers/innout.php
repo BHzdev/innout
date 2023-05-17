421bbcf3e9ec76ab7df598d08765bcaf95855b29
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-17 15:35:23 -0300

    add utils.php, some utility functions, starting error handling with messages

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -7,10 +7,22 @@ loadModel('WorkingHours');
 $user = $_SESSION['user'];
 $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
-$formatter = new IntlDateFormatter('pt_BR', IntlDateFormatter::NONE, IntlDateFormatter::SHORT, 'America/Sao_Paulo');
-$formatter->setPattern('HH:mm:ss');
-$currentTime = time();
-$formattedTime = $formatter->format($currentTime);
-$records->innout($formattedTime);
+try {
+  $formatter = new IntlDateFormatter('pt_BR', IntlDateFormatter::NONE, IntlDateFormatter::SHORT, 'America/Sao_Paulo');
+  $formatter->setPattern('HH:mm:ss');
+  $currentTime = time();
+  $formattedTime = $formatter->format($currentTime);
+  $records->innout($formattedTime);
+
+  $_SESSION['message'] = [
+    'type' => 'success',
+    'message' => 'Ponto inserido com sucesso'
+  ];
+} catch (AppException $e) {
+  $_SESSION['message'] = [
+    'type' => 'error',
+    'message' => $e->getMessage()
+  ];
+}
 
 header('Location: day_records.php');
