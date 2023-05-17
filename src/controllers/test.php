67c244780b800ea47f7dc5d103daf63312f8ac44
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-17 17:26:35 -0300

    add new method(getWorkedInterval) in WorkingHours, method return working time

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -3,3 +3,6 @@
 loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
+$workedInterval = $wh->getWorkedInterval()->format('%H:%I:%S');
+print_r($workedInterval);
+echo "<br>";
