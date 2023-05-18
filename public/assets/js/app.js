40cc92a0c33b2d8a30736db424bc0c43b6349467
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-18 15:37:13 -0300

    add two functions(addOneSecond and activeClock) in javascript to update hours in real time

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,27 @@
     body.classList.toggle('hide-sidebar');
   });
 })();
+
+function addOneSecond(hours, minutes, seconds) {
+  const date = new Date();
+  date.setHours(parseInt(hours));
+  date.setMinutes(parseInt(minutes));
+  date.setSeconds(parseInt(seconds) + 1);
+
+  const formattedHours = `${date.getHours()}`.padStart(2, '0');
+  const formattedMinutes = `${date.getMinutes()}`.padStart(2, '0');
+  const formattedSeconds = `${date.getSeconds()}`.padStart(2, '0');
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+function activeClock() {
+  const activeClock = document.querySelector('[active-clock]');
+  if (!activeClock) return;
+
+  setInterval(() => {
+    const parts = activeClock.innerHTML.split(':');
+    activeClock.innerHTML = addOneSecond(...parts);
+  }, 1000);
+}
+
+activeClock();
