38fe1a6a28a3e975d8b5306c5a947e687b902a30
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-18 15:16:59 -0300

    displaying the calculated hours to the user

--- src/config/loader.php
+++ src/config/loader.php
@@ -28,6 +28,8 @@ function loadTemplateView($viewName, $params = [])
   }
   $user = $_SESSION['user'];
   $workingHours = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+  $workedInterval = $workingHours->getWorkedInterval()->format('%H:%I:%S');
+  $exitTime = $workingHours->getExitTime()->format("H:i:s");
 
   require_once(TEMPLATE_PATH . "/header.php");
   require_once(TEMPLATE_PATH . "/left.php");
