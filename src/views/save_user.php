4c0fb5eea60208a85de92fb5763eee945ea6f6f4
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-19 20:15:04 -0300

    starting the development of the user registration and update page, add save_user controller and view

---
+++ src/views/save_user.php
@@ -0,0 +1,10 @@
+<main class="content">
+  <?php
+  renderTitle(
+    'Cadastro de usuário',
+    'Crie e atualize o usuário',
+    'icofont-user'
+  );
+  include(TEMPLATE_PATH . '/messages.php');
+  ?>
+</main>
