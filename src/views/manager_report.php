a44e4d30de3816f4b2599ba9c9606e7159630b99
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-19 19:23:11 -0300

    starting the development of the content of the management report page

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,9 +1,26 @@
 <main class="content">
   <?php
-  echo "Usuários Ativos : {$activeUsersCount}";
-  echo '<br>';
-  echo "Horas Trabalhadas no mes : {$hoursInMonth}";
-  echo '<br>';
-  print_r($absentUsers);
+  renderTitle(
+    'Relatório Gerencial',
+    'Resumo das horas trabalhadas dos funcionários',
+    'icofont-chart-histogram'
+  );
   ?>
+  <div class="summary-boxes">
+    <div class="summary-box bg-primary">
+      <i class="icofont-users"></i>
+      <p class="title">Qtde de Funcionários</p>
+      <h3 class="value"><?= $activeUsersCount ?></h3>
+    </div>
+    <div class="summary-box bg-danger">
+      <i class="icofont-patient-bed"></i>
+      <p class="title">Faltas</p>
+      <h3 class="value"><?= count($absentUsers) ?></h3>
+    </div>
+    <div class="summary-box bg-success">
+      <i class="icofont-sand-clock"></i>
+      <p class="title">Horas Trabalhadas no mês</p>
+      <h3 class="value"><?= $hoursInMonth ?></h3>
+    </div>
+  </div>
 </main>
