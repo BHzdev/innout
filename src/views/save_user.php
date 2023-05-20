08612ef7e8fb0fa75a5306d717e524bb5fada227
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-20 15:15:59 -0300

    refactoring update and insertion of the user in the controller, add method update in user model

--- src/views/save_user.php
+++ src/views/save_user.php
@@ -9,14 +9,15 @@
   ?>
 
   <form action="#" method="POST">
+    <input type="hidden" name="id" value="<?= $id ?>">
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="name">Nome</label>
-        <input type="text" id="name" name="name" placeholder="Informe o nome" class="form-control" value="<?= $user->name ?>">
+        <input type="text" id="name" name="name" placeholder="Informe o nome" class="form-control" value="<?= $name ?>">
       </div>
       <div class="form-group col-md-6">
         <label for="email">E-mail</label>
-        <input type="email" id="email" name="email" placeholder="Informe o email" class="form-control" value="<?= $user->email ?>">
+        <input type="email" id="email" name="email" placeholder="Informe o email" class="form-control" value="<?= $email ?>">
       </div>
     </div>
     <div class="form-row">
@@ -32,17 +33,17 @@
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="start_date">Data de Admissão</label>
-        <input type="date" id="start_date" name="start_date" class="form-control" value="<?= $user->start_date ?>">
+        <input type="date" id="start_date" name="start_date" class="form-control" value="<?= $start_date ?>">
       </div>
       <div class="form-group col-md-6">
         <label for="end_date">Data de Desligamento</label>
-        <input type="date" id="end_date" name="end_date" class="form-control" value="<?= $user->end_date ?>">
+        <input type="date" id="end_date" name="end_date" class="form-control" value="<?= $end_date ?>">
       </div>
     </div>
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="is_admin">Administrador?</label>
-        <input type="checkbox" id="is_admin" name="is_admin" class="form-control " <?= $user->is_admin ? 'checked' : '' ?>>
+        <input type="checkbox" id="is_admin" name="is_admin" class="form-control " <?= $is_admin ? 'checked' : '' ?>>
       </div>
     </div>
     <div>
